f7aec5f27caa604abb158b032e59e3896cd28051
Jenniffer Crystine Souza dos Santos <jenniffer.souza@iteris.com.br>
2021-07-31 13:20:04 -0300

    configura swagger

--- src/main/java/br/com/exemplo/eventos/configuration/SwaggerConfig.java
+++ src/main/java/br/com/exemplo/eventos/configuration/SwaggerConfig.java
@@ -1,15 +1,19 @@
 package br.com.exemplo.eventos.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 
@@ -17,7 +21,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.exemplo.eventos.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }
